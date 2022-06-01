7468272efe65ada849898fe666b7fe9354c0050d
runner <runner@Mac-1654097030656.local>
2022-06-01 15:26:23 +0000

    windows 1.1.0

--- Formula/windows.rb
+++ Formula/windows.rb
@@ -1,8 +1,9 @@
 class Windows < Formula
   desc "A simple tool for storing window configurations on macOS"
   homepage "https://www.github.com/lowski/windows"
-  url "https://github.com/lowski/windows/releases/download/v1.0.0/windows_1.0.0_universal.tar.gz"
-  sha256 "29e5daf46fb20c4601565571d6ec76de6d1e464abd26e79489fc84ae6db9c770"
+  url "https://github.com/lowski/windows/releases/download/v1.1.0/windows_1.1.0_universal.tar.gz"
+  version "1.1.0"
+  sha256 "aa3a7d13add09c577e339d12abfbb0319d2e3bdc7c039f0631c1891b5cf7d990"
 
   def install
     bin.install "windows"
